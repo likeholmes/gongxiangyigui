6cde253543486957f1cb096ad74de1be601464d8
likeholmes <slk4869@163.com>
2019-09-12 16:52:31 +0800

    add some controller5

--- src/main/java/com/shengchanshixi/gongxiangyigui/controller/FeedbackController.java
+++ src/main/java/com/shengchanshixi/gongxiangyigui/controller/FeedbackController.java
@@ -2,6 +2,11 @@ package com.shengchanshixi.gongxiangyigui.controller;
 
 import org.springframework.stereotype.Controller;
 
+/**
+ * Web
+ * 反馈控制器
+ */
 @Controller
 public class FeedbackController extends BaseController{
+
 }
